d9e802c9a1b00f6399f54095623be7c023a78639
JacKuna <kunalsaha290@yahoo.com>
2019-11-13 01:19:34 -0500

    Update deleteme.sh

--- deleteme.sh
+++ deleteme.sh
@@ -1,1 +1,1 @@
-mysql -ucyberkeeda -p${MysqlPass} mysql -e "update mytbl set f_title='$APVmode' where id=1;select f_title from mytbl"
+mysql -ucyberkeeda -predhat mysql -e "update mytbl set f_title='APVmode' where id=1;select f_title from mytbl"
